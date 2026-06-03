5c2ce8da0fd6f08570571168e00b32ff93661ecf
andreimagallanes77-code <andreimagallanes77@gmail.com>
2026-06-03 14:36:51 +0800

    Simulated car acceleration and braking by repeatedly updating and displaying the current speed, and fixing some common error. The program is now done.

--- test_car.py
+++ test_car.py
@@ -26,11 +26,21 @@ class Car:
     def brake(self):
         self.__speed -= 8
 
-    car1 = Car(2024, "Toyota")
+car1 = Car(2024, "Toyota")
 
-    print("Car Information")
-    print("Year Model:", car1.get_year_model())
-    print("Make:", car1.get_make())
+print("Car Information")
+print("Year Model:", car1.get_year_model())
+print("Make:", car1.get_make())
+print("Current Speed:", car1.get_speed())
+
+print("\nAccelerating...")
+for i in range(5):
+    car1.accelerate()
+    print("Current Speed:", car1.get_speed())
+
+print("\nBraking...")
+for i in range(5):
+    car1.brake()
     print("Current Speed:", car1.get_speed())
 
 
