d813ec1e413656880a83ec60f19b058ddc51f321
andreimagallanes77-code <andreimagallanes77@gmail.com>
2026-06-03 14:26:37 +0800

    Created a test Car object to verify that its attributes are initialized and displayed correctly.

--- test_car.py
+++ test_car.py
@@ -26,5 +26,12 @@ class Car:
     def brake(self):
         self.__speed -= 8
 
+    car1 = Car(2024, "Toyota")
+
+    print("Car Information")
+    print("Year Model:", car1.get_year_model())
+    print("Make:", car1.get_make())
+    print("Current Speed:", car1.get_speed())
+
 
 
