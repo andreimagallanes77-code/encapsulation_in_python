22063ab1edcbeb3033a2fb416edc255a99ca2e5f
andreimagallanes77-code <andreimagallanes77@gmail.com>
2026-06-04 20:38:43 +0800

    Created the first Car object for testing, the program now displays the car's basic information and starting speed.

--- pet_info.py
+++ pet_info.py
@@ -4,3 +4,12 @@ class Pet:
         self.__name = name
         self.__animal_type = animal_type
         self.__age = age
+
+    def get_year_model(self):
+        return self.__year_model
+
+    def get_make(self):
+        return self.__make
+
+    def get_speed(self):
+        return self.__speed
